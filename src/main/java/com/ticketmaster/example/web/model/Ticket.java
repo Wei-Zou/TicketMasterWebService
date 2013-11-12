57cf7a61f040904e0e594493d2b8dc6bc4dbd98b
Shulin Jia <s.lin.jia@gmail.com>
2013-11-11 23:32:04 -0800

    Member variables for Ticket object have been filled in

--- src/main/java/com/ticketmaster/example/web/model/Ticket.java
+++ src/main/java/com/ticketmaster/example/web/model/Ticket.java
@@ -1,6 +1,42 @@
 package com.ticketmaster.example.web.model;
 
-// TODO: waiting for Ticket object CSV
-public class Ticket {
-	// All members of the Ticket CSV sent to the data team...
+import java.io.Serializable;
+import java.util.Date;
+
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+
+// TODO: functions missing
+public class Ticket implements Serializable {
+	private String Moarid;
+	private String Inventoryticketid;
+	private String InvoiceDate;
+	private String TmEventID;
+	private String Productionid;
+	private String event_dt;
+	private String Event_Disp_Name;
+	private String Major_Cat_Name;
+	private String Minor_Cat_Name;
+	private String Venuename;
+	private String Venue_City;
+	private String Venue_State;
+	private String Seatsection;
+	private String Seatrow;
+	private String Seats;
+	private String Tickets;
+	private String Poamount;
+	private String Wholesaleamount;
+	private String Roundedretail;
+	private String Gtv;
+	private String Averagefacevalue;
+	private String itMinFV;
+	private String itMaxFV;
+	private String itTotFV;
+	private String bdMinlFV;
+	private String bdMaxFV;
+	private String bdTotFV;
+	private String MinLastSoldFV;
+	private String MaxLastSoldFV;
+	private String TotLastSoldFV;
+
 }
